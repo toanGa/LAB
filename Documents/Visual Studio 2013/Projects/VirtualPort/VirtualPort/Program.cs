f01c555fe5741719c408357286e0fb96c292bf0a
toanGa <trantoan20122582@gmail.com>
2016-11-07 10:08:56 +0700

    add all foder

---
+++ Documents/Visual Studio 2013/Projects/VirtualPort/VirtualPort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VirtualPort.BaiTapLon;
+using VirtualPort.Booling;
+using VirtualPort.BaiTapLon;
+using VirtualPort.UART;
+
+namespace VirtualPort
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UART.UART());
+        }
+    }
+}
